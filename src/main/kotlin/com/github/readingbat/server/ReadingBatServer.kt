6d91ee62af310b610cd78903acd62adff3e4f48a
pambrose <pambrose@mac.com>
2020-08-24 18:56:54 -0700

    Package cleanup

--- src/main/kotlin/com/github/readingbat/server/ReadingBatServer.kt
+++ src/main/kotlin/com/github/readingbat/server/ReadingBatServer.kt
@@ -193,7 +193,7 @@ internal fun Application.module() {
   routing {
     adminRoutes(metrics)
     locations(metrics) { content.get() }
-    userRoutes(metrics, { content.get() })
+    userRoutes(metrics) { content.get() }
     sysAdminRoutes(metrics, { content.get() }, { assignContentDsl(fileName, variableName) })
     wsEndpoints(metrics) { content.get() }
     static(STATIC_ROOT) { resources("static") }
