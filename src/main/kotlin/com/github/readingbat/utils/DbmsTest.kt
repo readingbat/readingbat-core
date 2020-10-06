f2d6399ef5b20ee8f5e71ad32788b6f349c20191
pambrose <pambrose@mac.com>
2020-10-06 14:55:34 -0700

    Add user query

--- src/main/kotlin/com/github/readingbat/utils/DbmsTest.kt
+++ src/main/kotlin/com/github/readingbat/utils/DbmsTest.kt
@@ -17,191 +17,20 @@
 
 package com.github.readingbat.utils
 
-import com.github.readingbat.server.BrowserSessions
-import com.github.readingbat.server.CustomConstant
-import com.github.readingbat.server.GeoInfos
-import com.github.readingbat.server.ServerRequests
-import com.github.readingbat.server.Users
-import com.github.readingbat.server.toRowString
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
-import org.jetbrains.exposed.dao.IntEntity
-import org.jetbrains.exposed.dao.IntEntityClass
-import org.jetbrains.exposed.dao.UUIDEntity
-import org.jetbrains.exposed.dao.UUIDEntityClass
-import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.dao.id.LongIdTable
-import org.jetbrains.exposed.dao.id.UUIDTable
-import org.jetbrains.exposed.sql.Count
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.Max
-import org.jetbrains.exposed.sql.SortOrder
-import org.jetbrains.exposed.sql.StdOutSqlLogger
-import org.jetbrains.exposed.sql.addLogger
-import org.jetbrains.exposed.sql.jodatime.CurrentDateTime
-import org.jetbrains.exposed.sql.jodatime.DateColumnType
-import org.jetbrains.exposed.sql.jodatime.datetime
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.transactions.transaction
-import java.util.*
-import kotlin.time.measureTime
-
-object Requests : LongIdTable() {
-  val created = datetime("created").defaultExpression(CurrentDateTime())
-  val sessionId = varchar("sessionId", 15)
-  val userId = varchar("userId", 25).index(customIndexName = "RequestsUserIdIndex")
-  val remote = text("remote")
-  val path = text("path")
-
-  override fun toString(): String = userId.toString()
-}
-
-object PUsers : UUIDTable() {
-  val created = varchar("created", 50)
-  val email = varchar("email", 50)
-  val first = varchar("first", 30)
-  val last = varchar("last", 30)
-  val salt = varchar("salt", 50)
-  val digest = varchar("digest", 50)
-  val enrolledClassCode = varchar("enrolledClassCode", 50)
-}
-
-class PUser(id: EntityID<UUID>) : UUIDEntity(id) {
-  var email by PUsers.email
-  var first by PUsers.first
-  var last by PUsers.last
-  var salt by PUsers.salt
-  var digest by PUsers.digest
-  var enrolledClassCode by PUsers.enrolledClassCode
-  override fun toString() = email
-
-  companion object : UUIDEntityClass<PUser>(PUsers)
-}
-
-object Cities : IntIdTable() {
-  val name = varchar("name", 50)
-}
-
-class City(id: EntityID<Int>) : IntEntity(id) {
-  var name by Cities.name
-  override fun toString() = name
-
-  companion object : IntEntityClass<City>(Cities)
-}
-
-/*
-abstract class Function<T>(override val columnType: IColumnType)
-   : ExpressionWithColumnType<T>()
-
-
-class FromUnixTime(private val unixTimeParam: Long)
-  : Function<DateTime>(DateColumnType(true)) {
-     override fun toQueryBuilder(queryBuilder: QueryBuilder): String
-         = "FROM_UNIXTIME($unixTimeParam)"
-
-     override val columnType: IColumnType = DateColumnType(true)
-   }
-*/
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
+import kotlin.time.milliseconds
 
 fun main() {
-  /*
-  Database.connect("jdbc:pgsql://localhost:5432/postgres",
-                   driver = "com.impossibl.postgres.jdbc.PGDriver",
-                   user = "postgres", password = "docker")
-   */
 
-  val postgres =
-    Database.connect(
-      HikariDataSource(
-        HikariConfig()
-          .apply {
-            driverClassName = "com.impossibl.postgres.jdbc.PGDriver"
-            maximumPoolSize = 5
-            isAutoCommit = false
-            transactionIsolation = "TRANSACTION_REPEATABLE_READ"
-            validate()
-          }))
+  val local = DateTime.now()
+  val utc = DateTime.now(DateTimeZone.UTC)
 
-  transaction {
-    // print sql to std-out
-    addLogger(StdOutSqlLogger)
+  println("local: $local")
+  println("utc: $utc")
+  println("diff: ${(utc.millis - local.millis)}")
+  println("diff: ${(utc.millis - local.millis).milliseconds}")
 
-    measureTime {
-      val session_id = BrowserSessions.session_id
-      val name = Users.fullName
-      val ip = GeoInfos.ip
-      val city = GeoInfos.city
-      val stateProv = GeoInfos.stateProv
-      val organization = GeoInfos.organization
-      val countryFlag = GeoInfos.countryFlag
-      val count = Count(Users.id)
-      val maxDate = Max(ServerRequests.created, DateColumnType(true))
-
-      (ServerRequests innerJoin BrowserSessions innerJoin Users innerJoin GeoInfos)
-        .slice(session_id, name, ip, city, stateProv, organization, countryFlag, count, maxDate)
-        .select { ServerRequests.created greater CustomConstant("now() - interval '2 day'", DateColumnType(true)) }
-        .groupBy(session_id, name, ip, city, stateProv, organization, countryFlag)
-        .orderBy(maxDate, SortOrder.DESC)
-        .forEach {
-          println(it.toRowString())
-        }
-    }.also { println("Took $it") }
-
-    /*
-    repeat(10) {
-      PasswordResets
-        .insert { row ->
-          row[userRef] = it.toLong()+2
-          row[updated] = DateTime(2020, 10, 31, 0, 0, 0)
-          row[resetId] = "resest id for user $it"
-          row[email] = "email value"
-        }
-      Thread.sleep(10000)
-    }
-     */
-  }
-
-  //transaction {
-  //addLogger(StdOutSqlLogger)
-
-  /*
-  PasswordResets
-    .slice(PasswordResets.created, CurrentDateTime(), CustomDateTimeConstant("NOW() - INTERVAL '1 hour'"))
-    .select { PasswordResets.created greater CustomDateTimeConstant("NOW() - INTERVAL '1 hour'") }
-    .forEach { row ->
-      val p = row[2]
-      println("${row[0]} ${row[1]} $p ${p.simpleClassName}")
-
-    }
-   */
-  //}
-  /*
-  //SchemaUtils.drop(Cities)
-  //SchemaUtils.create(Cities)
-  SchemaUtils.drop(Requests)
-  SchemaUtils.create(Requests)
-
-  val dur1 = measureTime {
-    repeat(10) { i ->
-      val stPete =
-        Requests.insertAndGetId { row ->
-          row[path] = "St. Petersburg $i"
-        }
-    }
-  }
-
-  val dur2 = measureTime {
-    println("Films: ${
-      Requests
-        .select { Requests.path neq "dd" }
-        .map {
-          "${it[Requests.id]} ${it[Requests.created]} ${it[Requests.path]}"
-        }.toList()
-    }")
-  }
-
-  println("$dur1 and $dur2")
-   */
 }
+
 
