822532a23e2acd3e839ea4422bd495c7ccf74607
pambrose <pambrose@mac.com>
2020-10-08 14:45:56 -0700

    Create 1.7.0 branch

--- src/main/kotlin/com/github/readingbat/server/ReadingBatServer.kt
+++ src/main/kotlin/com/github/readingbat/server/ReadingBatServer.kt
@@ -69,7 +69,7 @@ import kotlin.time.TimeSource
 import kotlin.time.measureTime
 import kotlin.time.seconds
 
-@Version(version = "1.6.0", date = "10/8/20")
+@Version(version = "1.7.0", date = "10/8/20")
 object ReadingBatServer : KLogging() {
   private val startTime = TimeSource.Monotonic.markNow()
   internal val serverSessionId = randomId(10)
