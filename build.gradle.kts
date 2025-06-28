6aee980fdf9b953271c0dfbbbab2a1fdad6bed31
pambrose <pambrose@mac.com>
2025-06-28 09:51:56 -0700

    Update gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,94 +1,27 @@
-/*
- * Copyright © 2025 Paul Ambrose (pambrose@mac.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-import jdk.tools.jlink.resources.plugins
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-buildscript {
-  dependencies {
-    classpath("org.postgresql:postgresql:${property("postgres_version")}")
-  }
-}
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
-  id("java")
-  id("application")
-  id("org.jetbrains.kotlin.jvm") version "1.9.22" apply false
-  id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
-  id("org.jmailen.kotlinter") version "5.1.1" apply false
-  id("com.github.gmazzo.buildconfig") version "5.6.7" apply false
-  id("com.github.ben-manes.versions") version "0.52.0" apply false
+  `java`
+  `application`
+  `java-library`
+  `maven-publish`
+
+  alias(libs.plugins.kotlin.jvm) apply true
+  alias(libs.plugins.kotlin.serialization) apply false
+  alias(libs.plugins.kotlinter) apply true
+  alias(libs.plugins.versions) apply true
+  alias(libs.plugins.buildconfig) apply false
+  // id("org.jetbrains.kotlinx.kover") version "0.5.0"
 }
 
-val libraries by extra {
-  mapOf(
-    "serialization" to "org.jetbrains.kotlinx:kotlinx-serialization-json:${property("serialization_version")}",
-    "core_utils" to "com.github.pambrose.common-utils:core-utils:${property("utils_version")}",
-    "ktor_server_utils" to "com.github.pambrose.common-utils:ktor-server-utils:${property("utils_version")}",
-    "ktor_client_utils" to "com.github.pambrose.common-utils:ktor-client-utils:${property("utils_version")}",
-    "script_utils_common" to "com.github.pambrose.common-utils:script-utils-common:${property("utils_version")}",
-    "script_utils_python" to "com.github.pambrose.common-utils:script-utils-python:${property("utils_version")}",
-    "script_utils_java" to "com.github.pambrose.common-utils:script-utils-java:${property("utils_version")}",
-    "script_utils_kotlin" to "com.github.pambrose.common-utils:script-utils-kotlin:${property("utils_version")}",
-    "service_utils" to "com.github.pambrose.common-utils:service-utils:${property("utils_version")}",
-    "prometheus_utils" to "com.github.pambrose.common-utils:prometheus-utils:${property("utils_version")}",
-    "exposed_utils" to "com.github.pambrose.common-utils:exposed-utils:${property("utils_version")}",
-    "prometheus_proxy" to "com.github.pambrose:prometheus-proxy:${property("proxy_version")}",
-    "simple_client" to "io.prometheus:simpleclient:${property("prometheus_version")}",
-    "script_engine" to "ch.obermuhlner:java-scriptengine:${property("java_script_version")}",
-    "css" to "org.jetbrains.kotlin-wrappers:kotlin-css:${property("css_version")}",
-    "ktor_client_core" to "io.ktor:ktor-client-core:${property("ktor_version")}",
-    "ktor_client_cio" to "io.ktor:ktor-client-cio:${property("ktor_version")}",
-    "ktor_server_core" to "io.ktor:ktor-server:${property("ktor_version")}",
-    "ktor_server_cio" to "io.ktor:ktor-server-cio:${property("ktor_version")}",
-    "ktor_server_auth" to "io.ktor:ktor-server-auth-jvm:${property("ktor_version")}",
-    "ktor_sessions" to "io.ktor:ktor-server-sessions:${property("ktor_version")}",
-    "ktor_rate_limit" to "io.ktor:ktor-server-rate-limit:${property("ktor_version")}",
-    "ktor_html" to "io.ktor:ktor-server-html-builder:${property("ktor_version")}",
-    "ktor_resources" to "io.ktor:ktor-server-resources:${property("ktor_version")}",
-    "ktor_metrics" to "io.ktor:ktor-server-metrics:${property("ktor_version")}",
-    "ktor_websockets" to "io.ktor:ktor-server-websockets:${property("ktor_version")}",
-    "ktor_compression" to "io.ktor:ktor-server-compression:${property("ktor_version")}",
-    "ktor_calllogging" to "io.ktor:ktor-server-call-logging:${property("ktor_version")}",
-    "khealth" to "dev.hayden:khealth:${property("khealth_version")}",
-    "hikari" to "com.zaxxer:HikariCP:${property("hikari_version")}",
-    "exposed_core" to "org.jetbrains.exposed:exposed-core:${property("exposed_version")}",
-    "exposed_jdbc" to "org.jetbrains.exposed:exposed-jdbc:${property("exposed_version")}",
-    "exposed_jodatime" to "org.jetbrains.exposed:exposed-jodatime:${property("exposed_version")}",
-    "pgjdbc" to "com.impossibl.pgjdbc-ng:pgjdbc-ng-all:${property("pgjdbc_version")}",
-    "postgres" to "org.postgresql:postgresql:${property("postgres_version")}",
-    "socket" to "com.google.cloud.sql:postgres-socket-factory:${property("cloud_version")}",
-    "gson" to "com.google.code.gson:gson:${property("gson_version")}",
-    "sendgrid" to "com.sendgrid:sendgrid-java:${property("sendgrid_version")}",
-    "commons" to "org.apache.commons:commons-text:${property("commons_version")}",
-    "flexmark" to "com.vladsch.flexmark:flexmark:${property("flexmark_version")}",
-    "github" to "org.kohsuke:github-api:${property("github_api_version")}",
-    "ktor_server_tests" to "org.jetbrains.kotlin:kotlin-test:${property("kotlin_version")}",
-    "ktor_server_test_host" to "io.ktor:ktor-server-test-host:${property("ktor_version")}",
-    "kotest_runner_junit5" to "io.kotest:kotest-runner-junit5:${property("kotest_version")}",
-    "kotest_assertions_core" to "io.kotest:kotest-assertions-core:${property("kotest_version")}",
-    "kotest_assertions_ktor" to "io.kotest:kotest-assertions-ktor:${property("kotest_ktor_version")}"
-  )
-}
+val versionStr: String by extra
+val kotlinLib = libs.plugins.kotlin.jvm.get().toString().split(":").first()
+val serializationLib = libs.plugins.kotlin.serialization.get().toString().split(":").first()
+val ktlinterLib = libs.plugins.kotlinter.get().toString().split(":").first()
 
 allprojects {
-  description = "ReadingBat Core"
-  group = "com.github.readingbat"
-  version = "2.0.5"
 
   apply(plugin = "application")
   apply(plugin = "java-library")
@@ -99,98 +32,165 @@ allprojects {
   apply(plugin = "com.github.gmazzo.buildconfig")
   apply(plugin = "com.github.ben-manes.versions")
 
+  extra["versionStr"] = "2.0.5"
+  description = "ReadingBat Core"
+  group = "com.github.readingbat"
+  version = versionStr
+
   repositories {
     google()
     mavenCentral()
     maven { url = uri("https://jitpack.io") }
   }
-
-//    sourceCompatibility = JavaVersion.VERSION_17
-//    targetCompatibility = JavaVersion.VERSION_17
-
-//    publishing {
-//        publications {
-//            create<MavenPublication>("mavenJava") {
-//                from(components["java"])
-//                versionMapping {
-//                    usage("java-api") {
-//                        fromResolutionOf("runtimeClasspath")
-//                    }
-//                    usage("java-runtime") {
-//                        fromResolutionResult()
-//                    }
-//                }
-//            }
-//        }
-//    }
 }
 
 subprojects {
-  dependencies {
-    // These are required for the annotation args below
-    //implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${property("coroutines_version")}")
-    //implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:${property("serialization_version")}")
+  configureKotlin()
+  configurePublishing()
+  configureTesting()
+  configureKotlinter()
 
-    implementation("io.github.oshai:kotlin-logging-jvm:${property("logging_version")}")
-    implementation("ch.qos.logback:logback-classic:${property("logback_version")}")
+//  dependencies {
+//    // These are required for the annotation args below
+//    //implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${property("coroutines_version")}")
+//    //implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:${property("serialization_version")}")
+//
+//    implementation(libs.logging)
+//    implementation(libs.logback)
+//
+//    runtimeOnly(libs.kotlin.scripting.jsr223)
+//  }
 
-    runtimeOnly("org.jetbrains.kotlin:kotlin-scripting-jsr223:${property("kotlin_version")}")
+//  tasks.register<Jar>("sourcesJar") {
+//    dependsOn(tasks.classes)
+//    from(sourceSets.main.get().allSource)
+//    archiveClassifier.set("sources")
+//  }
+//
+//  tasks.register<Jar>("javadocJar") {
+//    dependsOn(tasks.javadoc)
+//    from(tasks.javadoc.get().destinationDir)
+//    archiveClassifier.set("javadoc")
+//  }
+
+//  artifacts {
+//    archives(tasks.named("sourcesJar"))
+//    //archives(tasks.named("javadocJar"))
+//  }
+//
+//  java {
+//    withSourcesJar()
+//  }
+//
+//  kotlin {
+//    jvmToolchain(17)
+//  }
+
+//  tasks.withType<KotlinJvmCompile>().configureEach {
+//    compilerOptions {
+//      jvmTarget.set(JvmTarget.JVM_11)
+//      freeCompilerArgs.addAll(
+//        listOf(
+//          "-opt-in=kotlin.time.ExperimentalTime",
+//          "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+//          "-opt-in=kotlin.contracts.ExperimentalContracts",
+//          "-opt-in=kotlinx.coroutines.ObsoleteCoroutinesApi",
+//          "-opt-in=kotlin.ExperimentalStdlibApi",
+//          "-opt-in=kotlinx.coroutines.InternalCoroutinesApi",
+//          "-opt-in=kotlinx.coroutines.DelicateCoroutinesApi",
+//          "-opt-in=kotlin.concurrent.atomics.ExperimentalAtomicApi",
+//        )
+//      )
+//    }
+//  }
+
+
+//  tasks.withType<Test> {
+//    useJUnitPlatform()
+//
+//    testLogging {
+//      events("passed", "skipped", "failed", "standardOut", "standardError")
+//      exceptionFormat = TestExceptionFormat.FULL
+//      showStandardStreams = true
+//    }
+//  }
+//
+//  configure<org.jmailen.gradle.kotlinter.KotlinterExtension> {
+//    reporters = arrayOf("checkstyle", "plain")
+//  }
+}
+
+fun Project.configureKotlin() {
+  apply {
+    plugin(kotlinLib)
   }
 
-  tasks.register<Jar>("sourcesJar") {
-    dependsOn(tasks.classes)
-    from(sourceSets.main.get().allSource)
-    archiveClassifier.set("sources")
+  kotlin {
+    jvmToolchain(11)
   }
 
-  tasks.register<Jar>("javadocJar") {
-    dependsOn(tasks.javadoc)
-    from(tasks.javadoc.get().destinationDir)
-    archiveClassifier.set("javadoc")
+  tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions {
+      jvmTarget.set(JvmTarget.JVM_11)
+      freeCompilerArgs.addAll(
+        listOf(
+          "-opt-in=kotlin.time.ExperimentalTime",
+          "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+          "-opt-in=kotlin.contracts.ExperimentalContracts",
+          "-opt-in=kotlinx.coroutines.ObsoleteCoroutinesApi",
+          "-opt-in=kotlin.ExperimentalStdlibApi",
+          "-opt-in=kotlinx.coroutines.InternalCoroutinesApi",
+          "-opt-in=kotlinx.coroutines.DelicateCoroutinesApi",
+          "-opt-in=kotlin.concurrent.atomics.ExperimentalAtomicApi",
+        )
+      )
+    }
+  }
+}
+
+fun Project.configurePublishing() {
+  apply {
+    plugin("java-library")
+    plugin("maven-publish")
   }
 
-  artifacts {
-    archives(tasks.named("sourcesJar"))
-    //archives(tasks.named("javadocJar"))
+  publishing {
+    val versionStr: String by extra
+    publications {
+      create<MavenPublication>("maven") {
+        groupId = group.toString()
+        artifactId = project.name
+        version = versionStr
+        from(components["java"])
+      }
+    }
   }
 
   java {
     withSourcesJar()
   }
+}
 
-  kotlin {
-    jvmToolchain(17)
+fun Project.configureKotlinter() {
+  apply {
+    plugin(ktlinterLib)
   }
 
-  tasks.withType<KotlinCompile>().configureEach {
-    //'-Xlint:-options',
-    kotlinOptions {
-      freeCompilerArgs += listOf(
-        // "-Xbackend-threads=8",
-        "-opt-in=kotlin.time.ExperimentalTime",
-        "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
-        "-opt-in=kotlin.contracts.ExperimentalContracts",
-        "-opt-in=kotlinx.coroutines.ObsoleteCoroutinesApi",
-        "-opt-in=kotlin.ExperimentalStdlibApi",
-        "-opt-in=kotlinx.coroutines.InternalCoroutinesApi",
-        "-opt-in=kotlinx.coroutines.DelicateCoroutinesApi",
-        "-opt-in=kotlin.concurrent.atomics.ExperimentalAtomicApi",
-        // "-Xno-optimized-callable-references"
-      )
-    }
+  kotlinter {
+    ignoreFormatFailures = false
+    ignoreLintFailures = false
+    reporters = arrayOf("checkstyle", "plain")
   }
+}
 
-  tasks.withType<Test> {
+fun Project.configureTesting() {
+  tasks.test {
     useJUnitPlatform()
 
     testLogging {
       events("passed", "skipped", "failed", "standardOut", "standardError")
-      exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+      exceptionFormat = TestExceptionFormat.FULL
       showStandardStreams = true
     }
-  }
-
-  configure<org.jmailen.gradle.kotlinter.KotlinterExtension> {
-    reporters = arrayOf("checkstyle", "plain")
   }
 }
--- readingbat-core/build.gradle.kts
+++ readingbat-core/build.gradle.kts
@@ -1,20 +1,3 @@
-/*
- * Copyright © 2025 Paul Ambrose (pambrose@mac.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 description = "readingbat-core"
 
 // These are for the uber target
@@ -27,111 +10,88 @@ application {
 }
 
 dependencies {
-  implementation(libraries["serialization"]!!)
+  implementation(libs.serialization)
 
-  implementation(libraries["core_utils"]!!)
+  implementation(libs.core.utils)
 
-  implementation(libraries["ktor_server_utils"]!!)
-  implementation(libraries["ktor_client_utils"]!!)
+  implementation(libs.ktor.server.utils)
+  implementation(libs.ktor.client.utils)
 
-  implementation(libraries["script_utils_common"]!!)
-  implementation(libraries["script_utils_python"]!!)
-  implementation(libraries["script_utils_java"]!!)
-  implementation(libraries["script_utils_kotlin"]!!)
+  implementation(libs.script.utils.common)
+  implementation(libs.script.utils.python)
+  implementation(libs.script.utils.java)
+  implementation(libs.script.utils.kotlin)
 
-  implementation(libraries["service_utils"]!!)
-  implementation(libraries["prometheus_utils"]!!)
-  implementation(libraries["exposed_utils"]!!)
+  implementation(libs.service.utils)
+  implementation(libs.prometheus.utils)
+  implementation(libs.exposed.utils)
 
-  implementation(libraries["prometheus_proxy"]!!)
+  implementation(libs.prometheus.proxy)
 
-  implementation(libraries["simple_client"]!!)
+  implementation(libs.simple.client)
 
-  implementation(libraries["script_engine"]!!)
+  implementation(libs.script.engine)
 
-  implementation(libraries["css"]!!)
+  implementation(libs.css)
 
-  implementation(libraries["ktor_server_core"]!!)
-  implementation(libraries["ktor_server_cio"]!!)
-  implementation(libraries["ktor_server_auth"]!!)
-  implementation(libraries["ktor_client_core"]!!)
-  implementation(libraries["ktor_client_cio"]!!)
+  implementation(libs.ktor.server.core)
+  implementation(libs.ktor.server.cio)
+  implementation(libs.ktor.server.auth)
+  implementation(libs.ktor.client.core)
+  implementation(libs.ktor.client.cio)
 
-  implementation(libraries["ktor_sessions"]!!)
-  implementation(libraries["ktor_rate_limit"]!!)
-  implementation(libraries["ktor_html"]!!)
-  implementation(libraries["ktor_metrics"]!!)
-  implementation(libraries["ktor_websockets"]!!)
-  implementation(libraries["ktor_compression"]!!)
-  implementation(libraries["ktor_calllogging"]!!)
-  implementation(libraries["ktor_resources"]!!)
+  implementation(libs.ktor.sessions)
+  implementation(libs.ktor.rate.limit)
+  implementation(libs.ktor.html)
+  implementation(libs.ktor.metrics)
+  implementation(libs.ktor.websockets)
+  implementation(libs.ktor.compression)
+  implementation(libs.ktor.calllogging)
+  implementation(libs.ktor.resources)
 
-//    implementation(libraries["khealth"]!!)
+//    implementation(libs.khealth)
 
-  implementation(libraries["hikari"]!!)
+  implementation(libs.hikari)
 
-  implementation(libraries["exposed_core"]!!)
-  implementation(libraries["exposed_jdbc"]!!)
-  implementation(libraries["exposed_jodatime"]!!)
+  implementation(libs.exposed.core)
+  implementation(libs.exposed.jdbc)
+  implementation(libs.exposed.jodatime)
 
-  implementation(libraries["pgjdbc"]!!)
-  implementation(libraries["socket"]!!)
+  implementation(libs.pgjdbc)
+  implementation(libs.socket)
 
-  runtimeOnly(libraries["postgres"]!!)
+  runtimeOnly(libs.postgres)
 
-  implementation(libraries["gson"]!!)
+  implementation(libs.gson)
 
-  implementation(libraries["sendgrid"]!!)
+  implementation(libs.sendgrid)
 
-  implementation(libraries["commons"]!!)
-  implementation(libraries["flexmark"]!!)
+  implementation(libs.commons)
+  implementation(libs.flexmark)
 
-  implementation(libraries["github"]!!)
+  implementation(libs.github)
 
-  testImplementation(libraries["ktor_server_tests"]!!)
-  testImplementation(libraries["ktor_server_test_host"]!!)
+  implementation(libs.logging)
+  implementation(libs.logback)
 
-  testImplementation(libraries["kotest_runner_junit5"]!!)
-  testImplementation(libraries["kotest_assertions_core"]!!)
-  testImplementation(libraries["kotest_assertions_ktor"]!!)
+  runtimeOnly(libs.kotlin.scripting.jsr223)
+
+  testImplementation(libs.kotlin.test)
+  testImplementation(libs.ktor.server.test.host)
+
+  testImplementation(libs.kotest.runner.junit5)
+  testImplementation(libs.kotest.assertions.core)
+  testImplementation(libs.kotest.assertions.ktor)
 
   testImplementation(project(":readingbat-kotest"))
 }
-
-publishing {
-  publications {
-    create<MavenPublication>("maven") {
-      groupId = "com.github.readingbat"
-      artifactId = project.name
-      version = project.version.toString()
-
-      from(components["java"])
-    }
-  }
-}
-
-//publishing {
-//    publications {
-//        create<MavenPublication>("mavenJava") {
-//            from(components["java"])
-//            versionMapping {
-//                usage("java-api") {
-//                    fromResolutionOf("runtimeClasspath")
-//                }
-//                usage("java-runtime") {
-//                    fromResolutionResult()
-//                }
-//            }
-//        }
-//    }
-//}
 
 buildConfig {
   packageName("com.github.readingbat")
 
   buildConfigField("String", "CORE_NAME", "\"${project.name}\"")
   buildConfigField("String", "CORE_VERSION", "\"${project.version}\"")
-  buildConfigField("String", "CORE_RELEASE_DATE", "\"4/14/25\"")
+  buildConfigField("String", "CORE_RELEASE_DATE", "\"6/28/25\"")
   buildConfigField("long", "BUILD_TIME", "${System.currentTimeMillis()}L")
 }
 
@@ -139,22 +99,7 @@ buildConfig {
 tasks.register("stage") {
   dependsOn("uberjar", "build", "clean")
 }
+
 tasks.named("build") {
   mustRunAfter("clean")
 }
-
-//tasks.register<Jar>("uberjar") {
-//    dependsOn("shadowJar")
-//    zip64 = true
-//    archiveFileName.set("server.jar")
-//    manifest {
-//        attributes(
-//            "Implementation-Title" to appName,
-//            "Implementation-Version" to version,
-//            "Built-Date" to java.util.Date(),
-//            "Built-JDK" to System.getProperty("java.version"),
-//            "Main-Class" to mainName
-//        )
-//    }
-//    from(zipTree(tasks.named("shadowJar").get().outputs.files.singleFile))
-//}
