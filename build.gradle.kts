c8c464d3c268daab590a1955b79205edfd230339
pambrose <pambrose@mac.com>
2025-08-13 23:25:21 -0700

    Update library versions and Travis configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ allprojects {
   apply(plugin = "com.github.gmazzo.buildconfig")
   apply(plugin = "com.github.ben-manes.versions")
 
-  extra["versionStr"] = "2.1.1-SNAPSHOT"
+  extra["versionStr"] = "2.1.1"
   description = "ReadingBat Core"
   group = "com.github.readingbat"
   version = versionStr
